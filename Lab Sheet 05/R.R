264258bc3e1f824d994ee809858af7163f15e504
IT24100087 <IT24100087@my.sliit.lk>
2025-08-28 14:52:50 +0530

    Add files via upload

---
+++ Lab Sheet 05/R.R
@@ -0,0 +1,27 @@
+setwd("C:\\users\\IT24100087\\Desktop\\IT24100087")
+
+Delivery_Times<-read.table("Exercise - Lab 05.txt",header=TRUE) #import data set
+print(Delivery_Times)
+
+
+hist(Delivery_Times$Delivery,
+     breaks = seq(20, 70, by = 5),
+     right = FALSE,
+     main = "Histogram of Delivery Times",
+     xlab = "Delivery Times",
+     ylab = "Frequency")
+
+
+hist_data <- hist(Delivery_Times$Delivery,
+                  breaks = seq(20, 70, by = 5),
+                  right = FALSE,
+                  plot = FALSE)
+
+cumulative_freq <- cumsum(hist_data$counts)
+
+plot(hist_data$mids, cumulative_freq,
+     type = "o",
+     main = "Cumulative Frequency Polygon (Ogive)",
+     xlab = "Delivery Times",
+     ylab = "Cumulative Frequency",
+     pch = 16)
